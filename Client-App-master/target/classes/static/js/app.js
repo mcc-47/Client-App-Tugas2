b04250245bd1c8273341d85588afde61685b2ccc
williamyangjaya <williamyangjaya@gmail.com>
2021-04-15 00:34:13 +0700

    Salah push sebelumnya

--- Client-App-master/target/classes/static/js/app.js
+++ Client-App-master/target/classes/static/js/app.js
@@ -1,10 +1,5 @@
-// tabel
-$(document).ready(function () {
-    $('#table_id').DataTable();
-});
-
-// validation setiap form
-$(document).ready(function () {
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
     'use strict';
     window.addEventListener('load', function () {
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -20,106 +15,32 @@ $(document).ready(function () {
             }, false);
         });
     }, false);
-});
+})();
 
-//login
 $(document).ready(function () {
-    $("#loginForm").click(function () { // hides all element H1
-        var username = $("#username").val();
-        var password = $("#password").val();
-
-        if (username == '' || password == '') {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong!',
-                footer: '<a href>Why do I have this issue?</a>'
-            })
-        } else {
-            Swal.fire({
-                icon: 'success',
-                title: 'Okay',
-                text: 'Login Success',
-                footer: '<a href>Why do I have this issue?</a>'
-            })
-        }
-    });
+    $('#table_id').DataTable();
 });
 
-// logout di header
-$(document).ready(function () {
-    $("#logoutButton").on("click", function () {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.value === true) {
-                $('#logoutform').submit()
-            }
-        })
-    });
-});
+Swal.fire('Any fool can use a computer')
 
-
-//delete di index.html
-function cobadelete() {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                    )
-        }
-    })
-}
-
-//$("#addForm").on("submit", function(e) {
-//  e.preventDefault(); // cancel submission
-//  window.location.replace("http://localhost:8080/contacts");
-////  alert("this could redirect to login.php"); 
-//});
-
-
-////$(document).ready(function () {
-//    $("#inputForm").click(function(){ // hides all element H1
-//        $(this).hide(); // hides the current element
-//    });
-//});
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-
-//$(document).ready(function () {
-//    $("#loginForm").click(function () {
-//        var username = $("#username").val();
-//        var password = $("#password").val();
-//
-//        if (username == '' || password == '') {
-//            swal({
-//                title: "Fields Empty!!",
-//                text: "Please check the missing field!!",
-//                icon: "warning",
-//                button: "Ok",
-//            });
-//        } else {
-//
+//const tombol = document.querySelector('#login');
+//const a = tombol.addEventListener('click', function () {
+//    Swal.mixin({
+//        toast: true,
+//        position: 'top-end',
+//        showConfirmButton: false,
+//        timer: 3000,
+//        timerProgressBar: true,
+//        didOpen: (toast) => {
+//            toast.addEventListener('mouseenter', Swal.stopTimer)
+//            toast.addEventListener('mouseleave', Swal.resumeTimer)
 //        }
-//    });
+//    })
 //});
-
-
-
-
+//
+//
+//
+//Toast.fire({
+//    icon: 'success',
+//    title: 'Signed in successfully'
+//})
